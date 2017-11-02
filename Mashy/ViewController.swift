4fd4c0e4b2823c531c04ce28cffee0ce6aa16e94
amany <monna.elhadary@gmail.com>
2017-11-01 18:07:03 -0700

    no message

--- Mashy/ViewController.swift
+++ Mashy/ViewController.swift
@@ -29,6 +29,13 @@ class ViewController: UIViewController {
 
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden =  true
+        navigationItem.title = ""
+        //Change status bar color
+        let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+        statusBar.backgroundColor = UIColor(rgb:0xf57c00)
+    }
 
 
     func open_side_menu(){
@@ -46,4 +53,20 @@ class ViewController: UIViewController {
 
 
 }
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
 
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
